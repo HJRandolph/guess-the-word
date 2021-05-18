a0e47cb75d004440844b6983016dbcd7343332cd
HJRandolph <hjrandolph@gmail.com>
2021-05-18 17:19:50 -0400

    First commit. Creating variables for guess the word game.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessed = document.querySelector(".guessed-letters");
+const btn = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const progress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const appear = document.querySelector(".remaining > span");
+const messages = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+const word = 'magnolia';
+
