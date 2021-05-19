ac2381c9fbaa2ecb2fd17d92467dc8176c4be1ad
HJRandolph <hjrandolph@gmail.com>
2021-05-19 15:01:22 -0400

    Updating letters in word based on user input. Checking that letters are letters not numbers or symbols.:

--- js/script.js
+++ js/script.js
@@ -5,27 +5,31 @@ const progress = document.querySelector(".word-in-progress");
 const remaining = document.querySelector(".remaining");
 const appear = document.querySelector(".remaining > span");
 const messages = document.querySelector(".message");
+const test = document.querySelector(".test");
 const playAgain = document.querySelector(".play-again");
-const word = 'MAGNOLIA';
+const word = 'magnolia';
 const guessedLetters = [];
 
-const wordsInProgress = function(word){
+const wordInProgress = function(word){
     const pieces = [];
     for(const letter of word){
-        // console.log(letter);
+        
         pieces.push("●");
     }
     progress.innerText = pieces.join("");   
 };
 
+wordInProgress(word);
+
 btn.addEventListener("click", function(e){
     e.preventDefault();
-    let guess = letter.value;
-    messages.innerText = 
-    letter.value = "";
+    const guess = letter.value;
     acceptGuess(guess);
     console.log(guess);
+    letter.value = "";
 });
+
+console.log(guessedLetters);
 
 const acceptGuess = function(input){
     const acceptedLetter = /[a-zA-Z]/;
@@ -36,24 +40,48 @@ const acceptGuess = function(input){
     } else if (!input.match(acceptedLetter)){
         messages.innerText = "You have entered a non-alphabetic character. Please try again.";
     } else {
-        messages.innerText = "Well done, you!";
         makeGuess(input);
+        return input;
     }
     
 };
 
 const makeGuess = function(l){
     l = l.toUpperCase();
-    if (guessedLetters.length === 0){
-        guessedLetters.push(l);
-    }
-    if (!l.match(guessedLetters)){
-        guessedLetters.push(l);
-    } else {
-        messages.innerText = "You have already guessed that letter. Try again.";
-    }
-    
+   if(guessedLetters.includes(l)){
+       messages.innerText = "You have already guessed that letter. Try again.";
+   } else{
+       guessedLetters.push(l);
+       showGuessedLetters();
+       updatePage(guessedLetters);
+   }
     console.log(guessedLetters);
 };
 
-wordsInProgress(word);
+const showGuessedLetters = function(){
+    guessed.innerHTML = "";
+    for (const letter of guessedLetters){
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessed.append(li);
+    }
+};
+
+const updatePage = function(guessedLetters){
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const showWord = [];
+    for (const letter of wordArray){
+    if (guessedLetters.includes(letter)){
+        showWord.push(letter.toUpperCase());
+    } else {
+        showWord.push("●");
+    }
+    progress.innerText = showWord.join("");
+}
+// console.log(wordArray);
+
+};
+
+
+updatePage(guessedLetters);
